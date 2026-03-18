e2345315f06db2520813474bc915d9cef4b24b38
luisleon7677 <luisleon7677@gmail.com>
2026-03-18 13:25:54 -0500

    pwa sistemn

--- front/vite.config.js
+++ front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Sales Monitor - Monitor Ventas',
         short_name: 'SalesMonitor',
